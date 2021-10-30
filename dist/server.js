7bbb10da4938f9dc3b08af7f573fabf9dad2f119
afif0808 <afif.panai@gmail.com>
2021-10-30 10:55:16 +0700

    Apply typescript to project

---
+++ dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+var express = require('express');
+var app = express();
+var port = 434;
+var routes = require('./api/routes');
+routes(app);
+app.listen(port, function () {
+    console.log("Server started on port :" + port);
+});
