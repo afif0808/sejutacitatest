8f8f7d0f8c6c7ad109308d970c73c2255236c447
afif0808 <afif.panai@gmail.com>
2021-10-31 08:01:13 +0700

    Modify user model

--- model/user.ts
+++ model/user.ts
@@ -1,18 +1,22 @@
 import { ObjectId } from "bson";
+var bcrypt = require("bcrypt")
+
 
 class User {
     declare _id: ObjectId
     declare name: string
     declare email: string
     declare password: string
+    declare passwordSalt: string
     constructor() {
 
     }
 
 
     toJSONString(): string {
-        var json: any = this
+        var json: any = JSON.parse(JSON.stringify(this))
         delete json["password"]
+        delete json["passwordSalt"]
         json = JSON.stringify(json)
         json = json.replace("_id", "id")
         return json
@@ -24,7 +28,7 @@ class CreateUserPayload {
     declare email: string
     declare password: string
 
-    constructor(payload : any) {
+    constructor(payload: any) {
         this.name = payload.name
         this.email = payload.email
         this.password = payload.password
@@ -32,12 +36,15 @@ class CreateUserPayload {
 
     toUser(): User {
         var user: User = new User()
-        
         user._id = new ObjectId()
         user.name = this.name
         user.email = this.email
-        user.password = this.password
-
+        try {
+            user.passwordSalt = bcrypt.genSaltSync(10)
+            user.password = bcrypt.hashSync(this.password, user.passwordSalt)
+        } catch (err) {
+            throw err
+        }
         return user
     }
 
