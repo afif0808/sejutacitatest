9b14dc237a2632419642231bd277a7a3034f865e
afif0808 <afif.panai@gmail.com>
2021-11-01 05:58:58 +0700

    Handle error

--- service/user_service.ts
+++ service/user_service.ts
@@ -1,13 +1,14 @@
 import { ObjectId } from 'bson'
 import { CreateUserPayload, User } from '../model/user'
-import query , {QueryFunc} from "../model/query"
+import query, { QueryFunc } from "../model/query"
+import customerror from '../model/customerror'
 
 
 
 interface Repository {
     insertUser(user: User): void
     getUserList(...queries: QueryFunc[]): Promise<User[] | undefined>
-    getUser(query: QueryFunc , ...queries : QueryFunc[]): Promise<User | undefined>
+    getUser(query: QueryFunc, ...queries: QueryFunc[]): Promise<User | undefined>
 }
 
 var repository: Repository
@@ -19,7 +20,7 @@ export default class UserService {
     async createUser(payload: CreateUserPayload): Promise<User | undefined> {
         try {
             var existing = await repository.getUser(query.emailFilterQuery(payload.email))
-            if(existing) throw "user with given email already exists"
+            if (existing) throw customerror.userEmailExistsError
 
             var user: User = payload.toUser()
             repository.insertUser(user)
@@ -31,6 +32,15 @@ export default class UserService {
     async getUserList(...queries: QueryFunc[]): Promise<User[] | undefined> {
         try {
             return repository.getUserList(...queries)
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async getUser(id: any): Promise<User | undefined> {
+        try {
+            var user = repository.getUser(query.idFilterQuery(id))
+            return user
         } catch (err) {
             throw err
         }
