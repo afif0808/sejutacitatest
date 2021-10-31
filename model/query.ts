a6f9491008eb3327c3df8ea88eded9081015381b
afif0808 <afif.panai@gmail.com>
2021-10-31 13:53:20 +0700

    Refactor query model

--- model/query.ts
+++ model/query.ts
@@ -1,14 +1,34 @@
-interface QueryFunc { (): {} }
+import { URLSearchParams } from 'url';
+import { Request } from 'express';
+
+interface Query {
+    filter: {}
+    pagination: {}
+}
+
+interface QueryFunc { (query: Query): void }
 
 export default {
     idFilterQuery: (id: any): QueryFunc => {
-        return (): {} => {
-            return { id: id }
+        return (query: Query) => {
+            query.filter["id"] = id
         }
     },
-    emailFilterQuery : (email : string) : QueryFunc => {
-        return (): {} => {
-            return { email: email }
+    emailFilterQuery: (email: any): QueryFunc => {
+        return (query: Query) => {
+            query.filter["email"] = email
+        }
+    },
+    httpRequestQuery: (req: Request): QueryFunc => {
+        return (query: Query) => {
+            var urlQuery = new URLSearchParams(req.url.split("?")[1])
+            urlQuery.forEach((v,k)=>{
+                if (k == "limit" || k == "page") {
+                    query.pagination[k] = parseInt(v)
+                    return
+                }
+                query.filter[k] = v
+            })
         }
     }
 }
