a7a3ddf778037ec8065b1847e5ec9f9d0d3da2b4
afif0808 <afif.panai@gmail.com>
2021-11-02 08:18:16 +0700

    Add auth middleware

---
+++ middleware/auth_middleware.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express"
+import customerror from "../model/customerror"
+import { RestResponse } from "../model/rest"
+import { RoleAccess } from "../model/role"
+import { User } from "../model/user"
+
+interface Service {
+    authenticate(token: string, requiredAccess?: RoleAccess): User
+}
+
+var service: Service
+
+
+export default class AuthMiddleware {
+    constructor(srv: Service) {
+        service = srv
+    }
+    // ACL-like authenticate middleware
+    authenticate(requiredAccess?: RoleAccess) {
+        return (req: Request, resp: Response, next: NextFunction) => {
+            try {
+                var token = req.header("authorization")
+                if (!token || !token.includes("Bearer ")) throw customerror.unauthorizedError
+                token = token.replace("Bearer ", "")
+                var user: User = service.authenticate(token,requiredAccess)
+                req["locals"] = { user: user }
+                next()
+            } catch (err) {
+                switch (err) {
+                    case customerror.unauthorizedError:
+                        new RestResponse(401, "Unauthorized", null).json(resp)
+                        break
+                }
+                return
+            }
+        }
+    }
+}
