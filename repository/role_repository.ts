6718cc7cfee3457a81fa162b4269a99f0a7bb250
afif0808 <afif.panai@gmail.com>
2021-11-03 16:49:08 +0700

    Impelemt update role

--- repository/role_repository.ts
+++ repository/role_repository.ts
@@ -1,16 +1,18 @@
 import { Db, FindOptions } from "mongodb";
 import { Role } from "../model/role";
 import { Query, QueryFunc } from "../model/query";
+import customerror from "../model/customerror";
+import { MongoDB } from "../database";
 
 const collectionName: string = "roles"
-var dbInstance: Db
 class RoleMongoRepository {
-    constructor(db: Db) {
-        dbInstance = db
+    declare db: MongoDB
+    constructor(db: MongoDB) {
+        this.db = db
     }
     async insertRole(role: Role) {
         try {
-            var collection = dbInstance.collection(collectionName)
+            var collection = this.db.writeConn.collection(collectionName)
             var obj: any = { ...role }
             obj._id = obj.id
             delete obj["id"]
@@ -44,7 +46,7 @@ class RoleMongoRepository {
             var query: Query = this.applyListQuery(queries)
             var roles: Role[] = []
 
-            var result = await dbInstance.collection(collectionName).find(query.filter, query.pagination).toArray()
+            var result = await this.db.readConn.collection(collectionName).find(query.filter, query.pagination).toArray()
             result?.map((el) => {
                 var role: Role = new Role()
                 role.id = el._id
@@ -61,14 +63,14 @@ class RoleMongoRepository {
     }
     async getRole(queryFunc: QueryFunc, ...queries: QueryFunc[]): Promise<Role | undefined> {
         try {
-            var query : Query = new Query()
+            var query: Query = new Query()
 
             queryFunc(query)
             queries.forEach((callback) => {
                 callback(query)
             })
 
-            var result = await dbInstance.collection(collectionName).findOne(query.filter)
+            var result = await this.db.readConn.collection(collectionName).findOne(query.filter)
             if (!result) return undefined
             var role = new Role()
             role.id = result._id
@@ -80,6 +82,37 @@ class RoleMongoRepository {
         }
     }
 
+
+    async updateRole(role: Role, queryFunc: QueryFunc, ...queries: QueryFunc[]) {
+        try {
+            var query: Query = new Query
+            queryFunc(query)
+            queries.forEach((callback) => {
+                callback(query)
+            })
+
+            if (query.filter["id"])
+                delete Object.assign(query.filter, { _id: query.filter["id"] })["id"]
+
+            var obj: any = role
+
+            delete obj["id"]
+
+            for (let k in obj) {
+                if (obj[k] == undefined) {
+                    delete obj[k]
+                }
+            }
+            console.log(query)
+            var result = await this.db.writeConn.collection(collectionName).updateMany(query.filter, { "$set": obj })
+            if (result.matchedCount < 1) {
+                throw customerror.notFoundError
+            }
+
+        } catch (err) {
+            throw err
+        }
+    }
 
 }
 
