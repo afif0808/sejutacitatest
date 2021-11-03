5e50a221ce742b2657762bff998f7bcac67c8bb4
afif0808 <afif.panai@gmail.com>
2021-11-03 16:47:34 +0700

    Implement update role

--- service/role_service.ts
+++ service/role_service.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from 'bson'
-import { CreateRolePayload, Role } from '../model/role'
+import { CreateRolePayload, Role, UpdateRolePayload } from '../model/role'
 import query, { QueryFunc } from "../model/query"
 import customerror from '../model/customerror'
 
@@ -9,11 +9,12 @@ interface RoleRepository {
     insertRole(role: Role): void
     getRoleList(...queries: QueryFunc[]): Promise<Role[] | undefined>
     getRole(query: QueryFunc, ...queries: QueryFunc[]): Promise<Role | undefined>
+    updateRole(role: Role, queryFunc: QueryFunc, ...queries: QueryFunc[]): Promise<void>
 }
 
 
 export default class RoleService {
-    declare roleRepo : RoleRepository
+    declare roleRepo: RoleRepository
     constructor(repo: RoleRepository) {
         this.roleRepo = repo
     }
@@ -26,6 +27,17 @@ export default class RoleService {
         try {
             var users = await this.roleRepo.getRoleList(...queries)
             return users
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async updateRole(payload: UpdateRolePayload): Promise<void> {
+        try {
+            var role = new Role()
+            role.name = payload.name
+            role.accesses = payload.accesses
+            await this.roleRepo.updateRole(role, query.idFilterQuery(payload.id))
         } catch (err) {
             throw err
         }
